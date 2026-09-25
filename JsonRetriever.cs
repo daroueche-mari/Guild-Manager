46d394ad29917f9c46920ec9ccbae3c823b941e9
Yannis-Sandoval <yannis.sandoval@laplateforme.io>
2026-09-25 13:35:02 +0200

    Feat : Modify Json Retriever to solely Retrieve Json files

--- JsonRetriever.cs
+++ JsonRetriever.cs
@@ -2,7 +2,7 @@ using System;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
-// Basic Adventurer generator
+// Basic Adventurer Data Retriever generator
 
 public struct BasicAdventurerData
 {
@@ -10,7 +10,6 @@ public struct BasicAdventurerData
     public List<string> classes {get; set;}
     public List<string> races {get; set;} 
     public PowerCurves power_curves {get; set;}
-    public string path_images {get; set;}
     public List<string> motivations {get; set;}
 }
 
@@ -32,57 +31,67 @@ public class PowerCurves
     public List<int> Priest {get; set;}
 }
 
-public class Adventurer
+public class BasicAdventurerDataRetriever
 {
     Random rnd = new Random();
     BasicAdventurerData data;
 
-    public string name {get; set;}
-    public string advClass {get; set;}
-    public string race {get; set;}
-    public List<int> powerCurve {get; set;}
-    public string pathImage {get; set;}
-    public string motivation {get; set;}
+    // public string name {get; set;}
+    // public string advClass {get; set;}
+    // public string race {get; set;}
+    // public List<int> powerCurve {get; set;}
+    // public string pathImage {get; set;}
+    // public string motivation {get; set;}
 
 
-    public Adventurer(BasicAdventurerData basicData)
+    public BasicAdventurerDataRetriever(BasicAdventurerData basicData)
     {
         this.data = basicData;
-        int raceChoice = rnd.Next(data.races.Count());
-        this.race = data.races[raceChoice];
-        this.name = ChooseName(raceChoice, data.names);
-        int classChoice = rnd.Next(data.classes.Count());
-        this.advClass = data.classes[classChoice];
-        this.powerCurve = ChoosePowerCurve(classChoice, data.power_curves);
-        // this.pathImage = "";
-        this.motivation = data.motivations[rnd.Next(data.motivations.Count())];
+        // this.race = data.races[rnd.Next(data.races.Count())];
+        // this.name = ChooseName(data.names);
+        // this.advClass = data.classes[rnd.Next(data.classes.Count())];
+        // this.powerCurve = ChoosePowerCurve(data.power_curves);
+        // this.pathImage = ChoosePathImage(data.path_images);
+        // this.motivation = data.motivations[rnd.Next(data.motivations.Count())];
     }
 
-    public string ChooseName(int raceChoice, Names listsNames)
+    public (string race, string name, string advClass, List<int> powerCurve, string pathImage, string motivation) ReturnBasicAdv()
     {
-        var properties = typeof(Names)
-            .GetProperties()
-            .Where(p => p.PropertyType == typeof(List<string>))
-            .ToList();
-        var property = properties[raceChoice];
+        string race = this.data.races[rnd.Next(this.data.races.Count())];
+        string name = ChooseName(this.data.names, race);
+        string advClass = this.data.classes[rnd.Next(this.data.classes.Count())];
+        List<int> powerCurve = ChoosePowerCurve(this.data.power_curves, advClass);
+        string pathImage = ChoosePathImage(race, advClass);
+        string motivation = this.data.motivations[rnd.Next(this.data.motivations.Count())];
+        return (race, name, advClass, powerCurve, pathImage, motivation);
+    }
+
+    public string ChooseName(Names listsNames, string race)
+    {
+        var property = typeof(Names).GetProperty(race);
         List<string> listNames = (List<string>)property.GetValue(listsNames);
         string finalName = listNames[rnd.Next(listNames.Count())];
         return finalName;
     }
 
-    public List<int> ChoosePowerCurve(int classChoice, PowerCurves listPowerC)
+    public List<int> ChoosePowerCurve(PowerCurves listPowerC, string advClass)
     {
-        var properties = typeof(PowerCurves)
-            .GetProperties()
-            .Where(p => p.PropertyType == typeof(List<int>))
-            .ToList();
-        var property = properties[classChoice];
+        var property = typeof(PowerCurves).GetProperty(advClass);
         List<int> finalPowerC = (List<int>)property.GetValue(listPowerC);
         return finalPowerC;
     }
+
+    public string ChoosePathImage(string race, string advClass)
+    {
+        string sourcePath = $"../Assets/Images/Personnages/Aventuriers/{race}/{advClass}";
+        int countFiles = System.IO.Directory.GetFiles(sourcePath, "*.*", SearchOption.AllDirectories).Count();
+        string finalImagePath =
+        $"../Assets/Images/Personnages/Aventuriers/{race}/{advClass}/Profile_{race}_{advClass}{rnd.Next(1, countFiles+1)}";
+        return finalImagePath;
+    }
 }
 
-// Special Adventurer Generator
+// Special Adventurer Data Retriever Generator
 
 public class SpecialAdventurersData
 {
@@ -101,38 +110,51 @@ public struct SpecialAdventurerData
     public string motivation {get; set;}
 }
 
-public class SpecialAdventurer
+public class SpecialAdventurerDataRetriever
 {
     Random rnd = new Random();
     SpecialAdventurersData specialData;
-    SpecialAdventurerData data;
+    SpecialAdventurersData data;
 
-    public string specialName {get; set;}
-    public string name {get; set;}
-    public string advClass {get; set;}
-    public string race {get; set;}
-    public List<int> powerCurve {get; set;}
-    public string path_image {get; set;}
-    public string motivation {get; set;}
+    // public string specialName {get; set;}
+    // public string name {get; set;}
+    // public string advClass {get; set;}
+    // public string race {get; set;}
+    // public List<int> powerCurve {get; set;}
+    // public string pathImage {get; set;}
+    // public string motivation {get; set;}
 
 
-    public SpecialAdventurer(string specialName, SpecialAdventurersData specialData)
+    public SpecialAdventurerDataRetriever(SpecialAdventurersData specialData)
     {
-        this.data = ChooseSpecialAdventurer(specialName, specialData);
-        this.race = data.race;
-        this.name = data.name;
-        this.advClass = data.advClass;
-        this.powerCurve = data.power_curve;
-        // this.pathImage = "";
-        this.motivation = data.motivation;
+        this.data = specialData;
+        // this.data = ChooseSpecialAdventurer(specialName, specialData);
+        // this.race = data.race;
+        // this.name = data.name;
+        // this.advClass = data.advClass;
+        // this.powerCurve = data.power_curve;
+        // this.pathImage = data.path_image;
+        // this.motivation = data.motivation;
     }
 
-    public SpecialAdventurerData ChooseSpecialAdventurer(string specialName, SpecialAdventurersData specialData)
+    public (string race, string name, string advClass, List<int> powerCurve, string pathImage, string motivation) ReturnSpecialAdv(string specialName)
     {
         var property = typeof(SpecialAdventurersData).GetProperty(specialName);
-        SpecialAdventurerData selectedSpecChar = (SpecialAdventurerData)property.GetValue(specialData);
+        SpecialAdventurerData selectedSpecChar = (SpecialAdventurerData)property.GetValue(this.data);
+        string race = selectedSpecChar.race;
+        string name = selectedSpecChar.name;
+        string advClass = selectedSpecChar.advClass;
+        List<int> powerCurve = selectedSpecChar.power_curve;
+        string pathImage = $"../Assets/Images/Personnages/Aventuriers Spéciaux/{specialName}";
+        string motivation = selectedSpecChar.motivation;
+        return (race, name, advClass, powerCurve, pathImage, motivation);
+    }
 
-        return selectedSpecChar;
+    public List<int> ReturnPowerCurve(List<int> power_curve)
+    {
+        List<int> powerCurve = new List<int> {};
+        foreach (int power in powerCurve) {powerCurve.Add(power);}
+        return powerCurve;
     }
 }
 
@@ -145,25 +167,28 @@ class Retrieve
     {
         BasicAdventurerData basicSource = RetrieveBasicData();
 
-        Adventurer adv1 = new Adventurer(basicSource);
-        Console.WriteLine(adv1.name);
-        Console.WriteLine(adv1.advClass);
-        Console.WriteLine(adv1.race);
-        Console.WriteLine(string.Join(", ", adv1.powerCurve));
-        // Console.WriteLine(adv1.powerCurve);
-        Console.WriteLine(adv1.motivation);
+        BasicAdventurerDataRetriever basicAdvRetriever = new BasicAdventurerDataRetriever(basicSource);
+        var basicAdv1 = basicAdvRetriever.ReturnBasicAdv();
+        Console.WriteLine(basicAdv1.name + ", " + basicAdv1.advClass + ", " + basicAdv1.race + ", " +
+                          basicAdv1.pathImage + ", " + basicAdv1.motivation);
+        Console.WriteLine(string.Join(", ", basicAdv1.powerCurve));
+        var basicAdv2 = basicAdvRetriever.ReturnBasicAdv();
+        Console.WriteLine(basicAdv2.name + ", " + basicAdv2.advClass + ", " + basicAdv2.race + ", " +
+                          basicAdv2.pathImage + ", " + basicAdv2.motivation);
+        Console.WriteLine(string.Join(", ", basicAdv2.powerCurve));
 
 
         SpecialAdventurersData specialSource = RetrieveSpecialData();
 
-        SpecialAdventurer adv2 = new SpecialAdventurer("Zhiyuan", specialSource);
-        Console.WriteLine(adv2.name);
-        Console.WriteLine(adv2.advClass);
-        Console.WriteLine(adv2.race);
-        Console.WriteLine(string.Join(", ", adv2.powerCurve));
-        Console.WriteLine(adv2.motivation);
-
-        
+        SpecialAdventurerDataRetriever specialAdvRetriever = new SpecialAdventurerDataRetriever(specialSource);
+        var specialAdv1 = specialAdvRetriever.ReturnSpecialAdv("Fuchou");
+        Console.WriteLine(specialAdv1.name + ", " + specialAdv1.advClass + ", " + specialAdv1.race + ", " +
+                          specialAdv1.pathImage + ", " + specialAdv1.motivation);
+        Console.WriteLine(string.Join(", ", specialAdv1.powerCurve));
+        var specialAdv2 = specialAdvRetriever.ReturnSpecialAdv("Zhiyuan");
+        Console.WriteLine(specialAdv2.name + ", " + specialAdv2.advClass + ", " + specialAdv2.race + ", " +
+                          specialAdv2.pathImage + ", " + specialAdv2.motivation);
+        Console.WriteLine(string.Join(", ", specialAdv2.powerCurve));
     }
 
     public static BasicAdventurerData RetrieveBasicData()
